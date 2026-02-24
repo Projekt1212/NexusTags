5843822957e60880b9802c40c63efdbe68d49461
Projekt1212 <87088452+Projekt1212@users.noreply.github.com>
2026-02-24 13:03:50 +0700

    Update NexusTags.java

--- src/main/java/org/fahri/nexusone/NexusTags.java
+++ src/main/java/org/fahri/nexusone/NexusTags.java
@@ -2,8 +2,12 @@ package org.fahri.nexusone.NexusTags;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
+import java.util.UUID;
 
-public class NexusTags extends JavaPlugin {
+public class NexusTags extends JavaPlugin implements Listener {
     private static NexusTags instance;
     private DatabaseManager dbManager;
     private ConfigManager configManager;
@@ -19,6 +23,7 @@ public class NexusTags extends JavaPlugin {
 
         getCommand("tag").setExecutor(new TagCommand(this));
         getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+        getServer().getPluginManager().registerEvents(this, this); // Registrasi event quit
 
         if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
             new TagExpansion(this).register();
@@ -31,6 +36,13 @@ public class NexusTags extends JavaPlugin {
         if (dbManager != null) dbManager.close();
     }
 
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        GUIListener.getFilterIndex().remove(uuid);
+        GUIListener.getCurrentPage().remove(uuid);
+    }
+
     public void reloadPlugin() {
         reloadConfig();
         this.configManager = new ConfigManager(this);
@@ -41,4 +53,5 @@ public class NexusTags extends JavaPlugin {
     public FileConfiguration getTagsConfig() { return configManager.getTagsConfig(); }
     public FileConfiguration getGuiConfig() { return configManager.getGuiConfig(); }
     public FileConfiguration getMsgsConfig() { return configManager.getMsgsConfig(); }
+    public FileConfiguration getConfirmationConfig() { return configManager.getConfirmationConfig(); } // Tambahkan ini
 }
